ebad0d3b46ae9d2c8f21c6d01c8853db84414496
VighneshBilgi <vhbilgi@gmail.com>
2022-08-06 18:36:23 +0530

    Added: UC3_Appending_Data_To_Linked_List

--- src/com/bridgelabz/linkedlist/LinkedList.java
+++ src/com/bridgelabz/linkedlist/LinkedList.java
@@ -5,8 +5,8 @@ public class LinkedList<E> {
     Node<E> head;
     Node<E> tail;
 
-    public void add(E data) {
-        Node<E> newNode = new Node<>(data);
+    public void add(E key) {
+        Node<E> newNode = new Node<>(key);
         if(head == null){
             head = newNode;
             tail = newNode;
@@ -20,7 +20,7 @@ public class LinkedList<E> {
         Node<E> temp = head;
 
         while(temp!=null){
-            System.out.print(temp.data);
+            System.out.print(temp.key);
             temp = temp.next;
             if(temp != null){
                 System.out.print("->");
@@ -29,9 +29,9 @@ public class LinkedList<E> {
 
     }
 
-    public void append(E data) {
+    public void append(E key) {
 
-        Node<E> newNode = new Node<>(data);
+        Node<E> newNode = new Node<>(key);
         if(tail== null){
             head = newNode;
             tail = newNode;
