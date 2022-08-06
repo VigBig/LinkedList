c8895cdf866881529e3dd14b41e364b3f31b3013
VighneshBilgi <vhbilgi@gmail.com>
2022-08-06 14:09:02 +0530

    Added: UC1_Simple_Linked_List

--- src/com/bridgelabz/linkedlist/LinkedListMain.java
+++ src/com/bridgelabz/linkedlist/LinkedListMain.java
@@ -6,6 +6,12 @@ public class LinkedListMain {
 
         System.out.println("Welcome to the Linked List Program");
 
+        LinkedList<Integer> linkedList = new LinkedList();
+        linkedList.add(70);
+        linkedList.add(30);
+        linkedList.add(56);
+        linkedList.display();
+
     }
 
 }
